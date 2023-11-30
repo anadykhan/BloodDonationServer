a24fad744dcadbaa05ef584b122082c63554034f
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2023-11-30 15:23:27 +0600

    Create request done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8080
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -24,6 +24,29 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('bloodDonationDB').collection('users')
+        const requestsCollection = client.db('bloodDonationDB').collection('requests')
+
+        app.get('/users', async(req, res) => {
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get(`/users/:email`, async(req, res) => {
+            const email = req.params.email
+            const result = await usersCollection.findOne({email: email})
+            res.send(result)
+        })
+
+        app.post('/requests', async(req, res) => {
+            const request = req.body
+            console.log(request)
+            const result = await requestsCollection.insertOne(request)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
